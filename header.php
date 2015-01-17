bad037f7c85a8e7fe5f28e02e0a0ba6ed554e108
Nithin David Thomas <webofnithin@gmail.com>
2015-01-18 00:23:10 +0530

    Menu and contact page

--- header.php
+++ header.php
@@ -42,37 +42,34 @@
 
 	<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
 
-		<div id="container">
+		<div id="container" class="<?php if (is_front_page() ) { echo 'front_page'; } ?>">
 
 			<header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
-
 				<div id="inner-header" class="full-width cf pad-small-lr pad-small-tb">
 
 					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<p id="logo" class="h1 d-1of7" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+					<a href="<?php echo home_url(); ?>" rel="nofollow" class="d-1of7 h-100 text-center logo-mK " ><img src="<?php echo home_url(); ?>/wp-content/themes/bones/library/images/logo-<?php if (is_front_page() ) { echo 'white'; }else{ echo 'gray'; } ?>.png"></a>
 
 					<?php // if you'd like to use the site description you can un-comment it below ?>
 					<?php // bloginfo('description'); ?>
 
 
-					<nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
+					<nav role="navigation" class="main-menu h-100 d-4of7 pull-right" itemscope itemtype="http://schema.org/SiteNavigationElement">
 						<?php 
-							$mK_navigation_class =  'nav top-nav cf';
-							if (is_front_page() ) {
-								$mK_navigation_class = $mK_navigation_class.' front_page';
-							}
+							
 							wp_nav_menu(array(
     					         'container' => false,                           // remove nav container
-    					         'container_class' => 'menu cf d-6of7',                 // class of container (should you choose to use it)
+    					        
     					         'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
-    					         'menu_class' => $mK_navigation_class,               // adding custom nav class
+    					         'menu_class' => 'nav top-nav cf',               // adding custom nav class
     					         'theme_location' => 'main-nav',                 // where it's located in the theme
     					         'before' => '',                                 // before the menu
         			               'after' => '',                                  // after the menu
         			               'link_before' => '',                            // before each link
         			               'link_after' => '',                             // after each link
         			               'depth' => 0,                                   // limit the depth of the nav
-    					         'fallback_cb' => ''                             // fallback function (if there is one)
+    					         'fallback_cb' => 'false',                             // fallback function (if there is one)
+    					         
 						)); ?>
 
 					</nav>
